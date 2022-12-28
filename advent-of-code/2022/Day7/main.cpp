390280f23a3d0952e62f7cc9d1be31aa3a659fa0
Akilax0 <killerilaksha@gmail.com>
2022-12-28 07:57:22 +0530

    DAy7

--- advent-of-code/2022/Day7/main.cpp
+++ advent-of-code/2022/Day7/main.cpp
@@ -1,4 +1,5 @@
 #include <cstring>
+#include <utility>
 #pragma GCC optimize("Ofast")
 #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
 #pragma GCC optimize("unroll-loops")
@@ -25,8 +26,7 @@ using namespace std;
  
 typedef long long ll;
 typedef long double ld;
-typedef pair<int,int> p32;
-typedef pair<ll,ll> p64;
+typedef pair<int,int> p32; typedef pair<ll,ll> p64;
 typedef pair<double,double> pdd;
 typedef vector<ll> v64;
 typedef vector<int> v32;
@@ -66,6 +66,30 @@ while ((pos = line.find(del)) != std::string::npos) {
 }
 
 */
+
+
+map<string,vector<string>> tree;
+map<string,int> s;
+
+void bfs(pair<string,int>ss){
+    if(s.find(ss.first)==s.end()){
+        //cout<<ss.first<<" "<<ss.second<<endl; 
+        cout<<ss.first<<": ";
+        for(auto j: tree[ss.first]){
+            cout<<j<<endl; 
+            if(s.find(j)==s.end()){
+                bfs(make_pair(j,0));
+            }
+            ss.second += s[j];
+        }
+        cout<<endl;
+
+        s.insert({ss.first,ss.second});
+    }
+}
+
+
+
 int main()
 {
     string line;
@@ -74,22 +98,85 @@ int main()
 
     file.open("input.txt");
 
-    map<string,vector<string>> tree;
-    map<string,int> s;
+    bool ls=false;
     
+    map<string,string> parent;
+    string dir;
     if(file.is_open()){
         while(getline(file,line)){
-            cout<<line<<endl;
+            if(ls){
+                ls = false;
+                continue;
+            }
+            //cout<<line<<endl;
             if(line[0]=='$'){
-                cout<<line.substr(2,2)<<" ";
+                //cout<<line.substr(2,2)<<" ";
                 if(line.substr(2,2)=="cd"){
-                    cout<<line.substr(5,line.length()-5);
+                    string out =  line.substr(5,line.length()-5);       
+                 //   cout<<"Change into dir:"<<dir;
+                    if(out!=".."){
+                        ls = true;
+                        dir = out;
+                    }else{
+                        dir = parent[dir];
+                    }
                 }
             }
-            cout<<endl;
+            else if(line.substr(0,3)=="dir"){
+                //cout<<"found directory: "<<line.substr(3,line.length()-3);
+                string fold = line.substr(4,line.length()-4);
+                tree[dir].push_back(fold);
+                parent.insert({fold,dir});
+                
+            }
+            else{
+                string del=" ";
+                size_t pos = 0;
+                int size;
+                string file;
+                string tok;
+                while ((pos = line.find(del)) != std::string::npos) {
+                    tok = line.substr(0, pos);
+                    size = stoi(tok);
+                    line.erase(0, pos + del.length());
+                }
+                file = line;
+                //cout<< "file  with name: "<<file<<" & size: "<<size;
+                tree[dir].push_back(file);
+                parent.insert({file,dir});
+                s.insert({file,size});
+
+            }
+            //cout<<endl;
         }
 
         file.close();
+
+        
+        cout<<"Tree: "<<endl;
+        for(auto i:tree){
+            cout<<i.first<<": ";
+            for(auto j:i.second){
+                cout<<j<<" ";
+            }
+            cout<<endl;
+        }
+        cout<<endl;
+        bfs(make_pair("/",0));
+        cout<<"Sizes: "<<endl;
+        for(auto i:s){
+            cout<<i.first<<": "<<i.second<<endl;
+        }
+
+
+        ll tot=0;
+        for(auto i: tree){
+            if(s[i.first]<100000){
+                tot +=s[i.first];
+            }
+        }
+        cout<<tot<<endl;
+        
     }
 
     return 0;
